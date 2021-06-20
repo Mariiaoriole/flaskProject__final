45c553567dfb86679923cd87dff51bd809bc4e79
Mariiaoriole <krivvosh@mail.ru>
2021-06-21 00:05:11 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template
+
+import requests
+
+import dateparser
+
+import config
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    try:
+        data = get_data()
+        cleaned_data = prepare_data(data)
+    except Exception as e:
+        return render_template('error.html', err=str(e))
+
+    return render_template('index.html', data=cleaned_data)
+
+
+def get_data():
+    rows_url = config.API_MAIN_URL + 'datasets/1982/rows'
+    count_url = config.API_MAIN_URL + 'datasets/1982/count'
+
+    r = requests.get(count_url, params={'api_key': config.API_KEY})
+    if not r.ok:
+        raise Exception('failed to get dataset rows count: {0}'.format(r.text))
+
+    limit = int(r.text)
+    skip = 0
+    step = 100
+
+    data = []
+    while len(data) < limit:
+        params = {
+            '$top': step,
+            '$skip': skip,
+            'api_key': config.API_KEY,
+        }
+
+        r = requests.get(rows_url, params=params)
+        if not r.ok:
+            raise Exception('failed to load rows: {0}'.format(r.text))
+
+        j = r.json()
+        for element in j:
+            data.append(element)
+
+        skip += step
+
+    return data
+
+
+def prepare_data(data):
+    result = []
+
+    for element in data:
+        if 'Cells' in element and element['Cells'] is not None:
+            cell = element['Cells']
+
+            if 'Quarter' in cell and 'AveragePerCapita' in cell:
+                result.append({
+                    'quarter_name': cell['Quarter'],
+                    'average_count': cell['AveragePerCapita'],
+                    'date': dateparser.parse(cell['Quarter']),
+                })
+
+    return result
+
+if __name__ == '__main__':
+    app.run()
---
+++ config.py
@@ -0,0 +1,2 @@
+API_KEY = '7ddca49c979788f00d105eca74fb30a5'
+API_MAIN_URL = 'https://apidata.mos.ru/v1/'
